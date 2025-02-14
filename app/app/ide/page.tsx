33f30fdefd6862e4b34ddd62c47e2fb89f1c70cd
WhyFenceCode <whyfencecode@gmail.com>
2025-02-13 22:08:55 -0800

    feat: added ide cards to bage allowing for fast navigation to recomended editors

--- app/app/ide/page.tsx
+++ app/app/ide/page.tsx
@@ -1,5 +1,6 @@
 import { BetaAlert } from "@/components/navigation/components/beta-alert"
 import { StepIndicator } from "@/components/navigation/components/step-indicator"
+import { IdeLink } from "@/components/navigation/components/ide-links"
 
 export default function Page() {
     return (
@@ -9,12 +10,8 @@ export default function Page() {
             <p className="font-light">This guide shows you how to choose and install an IDE. In programing an IDE is the text editor we use for our code. IDE's are a powerfull tool and the first step to starting our programing journey is getting one.</p>
             <StepIndicator>1</StepIndicator>
             <p className="font-light">The first step that we will tackle is choosing what IDE we want. This is a personal choice, and is mainly up to you, the user. Some great options for you to check out are:</p>
-            <p>- Microsoft Visual Studios Code</p>
-            <p>- Sublime Text</p>
-            <p></p>
-            <p></p>
-            <p>- Notepad ++</p>
-            <p className="font-light">In this tutorial, as in my personal code, we will use VS Code, due to its ease of use, great plugin support, and wide language compatibility.</p>
+            <IdeLink />
+            <p className="font-light">In this tutorial, as in my personal code, we will use VS Code, due to its ease of use, great plugin support, and wide language compatibility. Once you have done your research into editors, it is time to make your decision, and decide on an editor you think you will like.</p>
         </div>
     )
 }
