659082595d1a8627366ef26f205dd6b968720086
WhyFenceCode <whyfencecode@gmail.com>
2025-02-13 22:26:49 -0800

    feat: ide: switched to 3 recomeneded editors

--- components/navigation/components/ide-links.tsx
+++ components/navigation/components/ide-links.tsx
@@ -7,17 +7,24 @@ import {
 export function IdeLink() {
     return (
         <div className="w-full sm:flex justify-between align-center my-8">
-            <div className="sm:w-[48%] w-full mb-6 sm:mb-0">
+            <div className="sm:w-[32%] w-full mb-6 sm:mb-0">
                 <Link href="https://code.visualstudio.com/" rel="noopener noreferrer" target="_blank">
                     <Card className="w-full aspect-[5/3] flex items-center justify-center">
                         <h1 className="text-5xl sm:text-6xl text-foreground leading-loose">VS Code</h1>
                     </Card>
                 </Link>
             </div>
-            <div className="sm:w-[48%] w-full">
+            <div className="sm:w-[32%] w-full">
                 <Link href="https://www.sublimetext.com/" rel="noopener noreferrer" target="_blank">
                     <Card className="w-full aspect-[5/3] flex items-center justify-center">
                         <h1 className="text-5xl sm:text-6xl text-foreground leading-loose">Sublime</h1>
+                    </Card>
+                </Link>
+            </div>
+            <div className="sm:w-[32%] w-full">
+                <Link href="https://www.jetbrains.com/" rel="noopener noreferrer" target="_blank">
+                    <Card className="w-full aspect-[5/3] flex items-center justify-center">
+                        <h1 className="text-5xl sm:text-6xl text-foreground leading-loose">JetBrains</h1>
                     </Card>
                 </Link>
             </div>
